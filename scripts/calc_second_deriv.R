08872f5a8d4388004604b8124f58dcfe643ec881
crollinson <crollinson@gmail.com>
2017-06-07 15:18:19 -0400

    Ann's function to calculate the stability index
    Stability Index = sum of squares of the second derivate

---
+++ scripts/calc_second_deriv.R
@@ -0,0 +1,17 @@
+# Function from A. Raiho to do the sum of squares of the second deriviative (our stability index)
+
+calc.second.deriv <- function(dataset,h=1){
+  # biomassCI = dataset that you want to calculate the second deriv on
+  # h = timestep for the derivative; default = 1
+  
+  T.tot <- length(dataset) - h
+  
+  t1 <- h + 1
+  
+  second.deriv <- sum(((dataset[(t1:T.tot)+h]-2*dataset[(t1:T.tot)]+dataset[(t1:T.tot)-h])/((h*100)^2))^2)
+  
+  return(second.deriv)
+  
+}
+
+
