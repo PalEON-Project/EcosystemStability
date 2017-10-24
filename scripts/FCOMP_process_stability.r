4237cda50df3ce80a325bbce9c54bef0d15ddb9f
Andria Dawson <andria.dawson@gmail.com>
2017-10-24 13:28:08 -0600

    Updating FCOMP scripts

--- scripts/FCOMP_process_stability.r
+++ scripts/FCOMP_process_stability.r
@@ -6,23 +6,17 @@ library(grid)
 stepps_si = read.csv('data/Stability_STEPPS.csv')
 path.google <- "~/Google Drive/PalEON_ecosystem-change_models-vs-data"
 
-# convert coordinates to lat/long
-coordinates(stepps_si) <- ~ x + y
-proj4string(stepps_si) <- CRS('+init=epsg:3175')
-
-stepps_ll <- spTransform(stepps_si, CRS('+proj=longlat +ellps=WGS84'))
-stepps_ll <- data.frame(stepps_ll)
-
-
 # read in stability model annual
 fcomp_annual = read.csv('data/Stability_FCOMP_annual.csv')
 fcomp_annual$lat = paleon[fcomp_annual$cell, 'lat'] 
 fcomp_annual$lon = paleon[fcomp_annual$cell, 'lon'] 
+write.csv(fcomp_annual, 'data/Stability_FCOMP_annual.csv')
 
 # make some maps
 us <- map_data("state")
 models = as.vector(unique(fcomp_annual$model))
 
+# annual
 # png(file.path("figures", "Stability_FCOMP.png"), height=4, width=6, units="in", res=320)
 pdf(file.path("figures", "Stability_FCOMP.pdf"), height=4, width=6)
 for (model in models){
@@ -43,6 +37,39 @@ for (model in models){
       geom_path(data=us,aes(x=long, y=lat, group=group), color="gray50") + 
       coord_equal(xlim=range(fcomp_annual$lon), ylim=range(fcomp_annual$lat), expand=0) +
       scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = mean(fcomp_annual$n.yrs.sig, na.rm=T)) +
+      theme_bw() +
+      ggtitle(paste0("Number of Years Showing Change; ", model,": ", pft))
+    
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(2, 1)))
+    print(fcomp.deriv, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+    print(fcomp.sig, vp = viewport(layout.pos.row = 2, layout.pos.col = 1))
+  }
+}
+dev.off()
+
+
+# centennial
+fcomp_100 = read.csv('data/Stability_FCOMP_centennial.csv')
+pdf(file.path("figures", "Stability_FCOMP_100.pdf"), height=4, width=6)
+for (model in models){
+  pfts = unique(fcomp_100[which(fcomp_100$model == model),'pft'])
+  for (pft in pfts){
+    fcomp_sub = fcomp_100[which((fcomp_100$model==model)&(fcomp_100$pft== pft)),]
+    
+    fcomp.deriv <- ggplot(data=fcomp_sub) +
+      geom_tile(aes(x=lon, y=lat, fill=deriv.abs)) +
+      geom_path(data=us,aes(x=long, y=lat, group=group), color="gray50") + 
+      coord_equal(xlim=range(fcomp_100$lon), ylim=range(fcomp_100$lat), expand=0) +
+      scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = mean(fcomp_100$deriv.abs, na.rm=T)) +
+      theme_bw() +
+      ggtitle(paste0("Mean Absolute Rate of Change; ", model,": ", pft))
+    
+    fcomp.sig <- ggplot(data=fcomp_sub) +
+      geom_tile(aes(x=lon, y=lat, fill=n.yrs.sig)) +
+      geom_path(data=us,aes(x=long, y=lat, group=group), color="gray50") + 
+      coord_equal(xlim=range(fcomp_100$lon), ylim=range(fcomp_100$lat), expand=0) +
+      scale_fill_gradient2(low = "blue", high = "red", mid = "white", midpoint = mean(fcomp_100$n.yrs.sig, na.rm=T)) +
       theme_bw() +
       ggtitle(paste0("Number of Years Showing Change; ", model,": ", pft))
     
--- scripts/FCOMP_stability.r
+++ scripts/FCOMP_stability.r
@@ -1,4 +1,5 @@
-fcomp_coarse = readRDS('data/fcomp_coarse.RDS')
+mip_fcomp = readRDS('data/mip_fcomp.RDS')
+mip2coarse = read.csv('data/mip2coarse.csv')
 
 prob_sig <- function(x, prob){
   n_side = max(sum(x>0), sum(x<0))
@@ -10,6 +11,7 @@ prob_sig <- function(x, prob){
   }
 }
 
+load('data/input.rdata')
 rIts = readRDS('data/rIts_small.RDS')
 niter = dim(rIts)[4]
 
@@ -36,7 +38,7 @@ taxon_sig  = apply(taxon_deriv, c(2,3), function(x) sum(!is.na(x)))
 taxon_deriv_abs = apply(taxon_deriv, c(2,3), function(x) mean(abs(x), na.rm=TRUE))
 taxon_deriv_mean = apply(taxon_deriv, c(2,3), function(x) mean(x, na.rm=TRUE))
 
-
+library(reshape2)
 taxon_si = melt(taxon_deriv_mean)
 colnames(taxon_si) = c('cell', 'pft', 'deriv.mean')
 taxon_si$pft = taxa[taxon_si$pft]
@@ -45,12 +47,14 @@ taxon_si$sig = melt(taxon_sig)[,3]
 
 stepps_si = data.frame(x=rep(centers_veg[,1], times=2), 
                        y=rep(centers_veg[,2], times=2), 
+                       cell=rep(seq(1,nrow(centers_veg)), 2),
                        deriv.mean=c(decid_deriv_mean, ever_deriv_mean), 
                        deriv.abs=c(decid_deriv_abs, ever_deriv_abs), 
                        pft=c(rep('Deciduous', nrow(centers_veg)), rep('Evergreen', nrow(centers_veg))),
                        sig = c(decid_sig, ever_sig))
 stepps_si = rbind(stepps_si, data.frame(x=rep(centers_veg[,1], times=12),
                                         y=rep(centers_veg[,2], times=12),
+                                        cell=seq(1,nrow(centers_veg)),
                                         deriv.mean = taxon_si$deriv.mean,
                                         deriv.abs = taxon_si$deriv.abs,
                                         pft = taxon_si$pft,
@@ -58,6 +62,38 @@ stepps_si = rbind(stepps_si, data.frame(x=rep(centers_veg[,1], times=12),
 
 stepps_si$x = stepps_si$x*1e6 
 stepps_si$y = stepps_si$y*1e6 
+
+# convert coordinates to lat/long
+coordinates(stepps_si) <- ~ x + y
+proj4string(stepps_si) <- CRS('+init=epsg:3175')
+
+stepps_ll <- spTransform(stepps_si, CRS('+proj=longlat +ellps=WGS84'))
+stepps_ll <- data.frame(stepps_ll)
+
+stepps_save = stepps_ll[,c('cell', 'y', 'x', 'pft', 'deriv.mean', 'deriv.abs', 'sig')]
+colnames(stepps_save) = c('cell', 'lat', 'lon', 'pft', 'deriv.mean', 'deriv.abs', 'sig')
+
+rIts_mean = apply(rIts, c(1,2,3), mean, na.rm=TRUE)
+rIts_mean2 = apply(rIts_mean, c(1,2), mean, na.rm=TRUE)
+
+rIts_decid_mean = apply(rIts_decid, c(1,2), mean, na.rm=TRUE)
+rIts_decid_mean2 = apply(rIts_decid_mean, c(1), mean, na.rm=TRUE)
+
+rIts_ever_mean = apply(rIts_ever, c(1,2), mean, na.rm=TRUE)
+rIts_ever_mean2 = apply(rIts_ever_mean, c(1), mean, na.rm=TRUE)
+
+rIts_mean_melt = melt(rIts_mean2)
+colnames(rIts_mean_melt) = c('cell', 'pft', 'value')
+rIts_mean_melt$pft = taxa[rIts_mean_melt$pft]
+rIts_mean_melt = rbind(rIts_mean_melt,
+                       data.frame(cell=seq(1,nrow(centers_veg)), pft=rep('Deciduous', nrow(centers_veg)), value=rIts_decid_mean2),
+                       data.frame(cell=seq(1,nrow(centers_veg)), pft=rep('Evergreen', nrow(centers_veg)), value=rIts_decid_mean2))
+
+stepps_si = merge(stepps_save, rIts_mean_melt, by=c("cell", "pft"))
+
+# now save a csv file with columns: lon, lat, deriv.mean, deriv.abs, n.yrs, n.yrs.sig, fract.sig
+write.csv(stepps_si, 'data/Stability_STEPPS.csv')
+
 
 lims = list(xlims=c(min(paleon$x), max(paleon$x)), ylims=c(min(paleon$y), max(paleon$y)))
 
@@ -78,19 +114,32 @@ p <- p + facet_wrap(~pft) + theme_bw() + theme(axis.ticks=element_blank(), axis.
 print(p)
 ggsave('figures/maps_deriv_si_taxon.pdf')
 
-# now save a csv file with columns: lon, lat, deriv.mean, deriv.abs, n.yrs, n.yrs.sig, fract.sig
-write.csv(stepps_si, 'data/Stability_STEPPS.csv')
 
 # now do this for all of the models
 # for models, use gams
 # then do this at annual and centennial
-calc.stability <- function(x, width){
-  dat.tmp <- data.frame(Y=x, Year=1:length(x))
+# calc.stability <- function(x, width){
+#   dat.tmp <- data.frame(Y=x, Year=1:length(x))
+#   k.use=round(length(x)/width, 0)
+#   mod.gam <- gam(Y ~ s(Year, k=k.use), data=dat.tmp)
+#   mod.deriv <- calc.derivs(mod.gam, newdata=dat.tmp, vars="Year")
+#   return(mod.deriv)
+# }
+
+calc.stability <- function(x, width, res){
+  dat.tmp <- data.frame(Y=x, Year=seq(1, length(x)) )
+  if (res=="cent"){
+    idx = seq(1, length(x), by=100) 
+    dat.tmp.pred <- data.frame(Y=x[idx], Year=seq(1, length(x))[idx] )
+  } else {
+    dat.tmp.pred = dat.tmp
+  }
   k.use=round(length(x)/width, 0)
   mod.gam <- gam(Y ~ s(Year, k=k.use), data=dat.tmp)
-  mod.deriv <- calc.derivs(mod.gam, newdata=dat.tmp, vars="Year")
+  mod.deriv <- calc.derivs(mod.gam, newdata=dat.tmp.pred, vars="Year")
   return(mod.deriv)
 }
+
 
 library(mgcv)
 
@@ -98,6 +147,9 @@ paleon = read.csv('data/paleon_site_info.csv')
 
 mip_fcomp$x = paleon[match(mip_fcomp$cell_id, paleon$num), 'x']
 mip_fcomp$y = paleon[match(mip_fcomp$cell_id, paleon$num), 'y']
+
+# only want 850-1850
+mip_fcomp = mip_fcomp[which((mip_fcomp$year>=850)&(mip_fcomp$year<=1850)),]
 
 models = unique(mip_fcomp$model)
 
@@ -126,9 +178,11 @@ for (model in models){
     }
     
     for (n in 1:N_cells){
-      print(paste0('Cell ', n, ' of ', N_cells))
+      if ((n %% 50) == 0){
+        print(paste0('Cell ', n, ' of ', N_cells))
+      }
+        
       cell=cells[n]
-      
       x=x_pft[which((x_pft$model==model)&(x_pft$cell_id==cell)&(x_pft$pft==pft)), ]
       
       if (all(x$value==0)){
@@ -136,7 +190,7 @@ for (model in models){
         next
       }
       
-      stability = calc.stability(x$value, width=25)
+      stability = calc.stability(x$value, width=25, res="annual")
       nyears = nrow(stability)
       
       fcomp_si = rbind(fcomp_si, data.frame(model=rep(model, nyears), 
@@ -152,6 +206,7 @@ for (model in models){
 }
 
 fcomp_si$sig.bool = !is.na(fcomp_si$sig)
+write.csv(fcomp_si, 'data/Stability_FCOMP_annual_by_year.csv')
 
 deriv.abs  = aggregate(deriv ~ cell + pft + model, fcomp_si, function(x) mean(abs(x), na.rm=TRUE))
 deriv.mean = aggregate(deriv ~ cell + pft + model, fcomp_si, function(x) mean(x, na.rm=TRUE))
@@ -165,6 +220,14 @@ fcomp_si_summary = data.frame(deriv.abs[,1:3],
                               n.yrs = n.yrs$year, 
                               n.yrs.sig = n.yrs.sig$sig.bool, 
                               fract.sig = fract.sig)
+
+mip_fcomp_mean = aggregate(value~model+cell_id+pft, mip_fcomp, mean, na.rm=TRUE)
+colnames(mip_fcomp_mean) = c('model', 'cell', 'pft', 'value')
+
+# library(reshape2)
+# mip_fcomp_mean_cast = dcast(mip_fcomp_mean, model+cell_id~pft)
+
+fcomp_si_summary = merge(fcomp_si_summary, mip_fcomp_mean)
 
 write.csv(fcomp_si_summary, 'data/Stability_FCOMP_annual.csv')
 
@@ -204,7 +267,7 @@ for (model in models){
         next
       }
       
-      stability = calc.stability(x$value, width=100)
+      stability = calc.stability(x$value, width=100, res="cent")
       nyears = nrow(stability)
       
       fcomp_si_100 = rbind(fcomp_si_100, data.frame(model=rep(model, nyears), 
@@ -220,6 +283,7 @@ for (model in models){
 }
 
 fcomp_si_100$sig.bool = !is.na(fcomp_si_100$sig)
+write.csv(fcomp_si_100, 'data/Stability_FCOMP_centennial_by_year.csv')
 
 deriv.abs  = aggregate(deriv ~ cell + pft + model, fcomp_si_100, function(x) mean(abs(x), na.rm=TRUE))
 deriv.mean = aggregate(deriv ~ cell + pft + model, fcomp_si_100, function(x) mean(x, na.rm=TRUE))
@@ -233,6 +297,17 @@ fcomp_si_summary = data.frame(deriv.abs[,1:3],
                               n.yrs = n.yrs$year, 
                               n.yrs.sig = n.yrs.sig$sig.bool, 
                               fract.sig = fract.sig)
+
+fcomp_si_summary$lat = paleon[fcomp_si_summary$cell, 'lat'] 
+fcomp_si_summary$lon = paleon[fcomp_si_summary$cell, 'lon'] 
+
+mip_fcomp_mean = aggregate(value~model+cell_id+pft, mip_fcomp, mean, na.rm=TRUE)
+colnames(mip_fcomp_mean) = c('model', 'cell', 'pft', 'value')
+
+# library(reshape2)
+# mip_fcomp_mean_cast = dcast(mip_fcomp_mean, model+cell_id~pft)
+
+fcomp_si_summary = merge(fcomp_si_summary, mip_fcomp_mean)
 
 write.csv(fcomp_si_summary, 'data/Stability_FCOMP_centennial.csv')
 
