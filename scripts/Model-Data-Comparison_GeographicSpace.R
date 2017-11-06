c4987e62363c9c88a2fb2ce4803532e51725ee9b
Christy Rollinson <crollinson@gmail.com>
2017-11-06 14:56:46 -0600

    Model v. Data: Geographic space linear regressions
    Add scatter plots showing STEPPS and ReFAB stability compared to the
    closest model grid cell.  In general there is 0 correlation.  Only sig
    correlation found is LINKAGES vs. ReFAB biomass

--- scripts/Model-Data-Comparison_GeographicSpace.R
+++ scripts/Model-Data-Comparison_GeographicSpace.R
@@ -10,6 +10,8 @@ rm(list=ls())
 # Load Libaries & Set file paths
 # -------------------------------------------
 library(ggplot2); library(gridExtra); library(scales)
+library(grid)
+
 library(sp)
 
 # Path to where the raw output is
@@ -33,6 +35,17 @@ lbda$type <- as.factor("empirical")
 lbda$resolution <- as.factor("annual")
 names(lbda)[which(names(lbda)=="n.yrs.sig")] <- "n.sig"
 summary(lbda)
+
+us <- map_data("state")
+
+# png(file.path(path.google, "Current Figures/Stability_Synthesis", "PDSI_LBDA_TimeSeriesDepth.png"), height=5, width=8, units="in", res=320)
+# ggplot(data=lbda) +
+#   geom_tile(aes(x=lon, y=lat, fill=n.yrs)) +
+#   geom_path(data=us,aes(x=long, y=lat, group=group), color="gray25") +
+#   ggtitle("Time Series Length: LBDA") +
+#   coord_equal(xlim=range(lbda$lon), ylim=range(lbda$lat), expand=0) +
+#   theme_bw() 
+# dev.off()
 # -----------
 
 # -----------
@@ -57,14 +70,16 @@ summary(drivers)
 # -----------
 # STEPPS (empirical composition, centennially-resolved)
 # -----------
-stepps <- read.csv(file.path(path.google, "Current Data/Stability_GAMs", "Stability_STEPPS2.csv"))
+stepps <- read.csv(file.path(path.google, "Current Data/Stability_GAMs", "Stability_STEPPS.csv"))
 
 # Need to convert this to latlon
-albers <- sp::CRS("+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") # Define the spatial projection
-stepps <- sp::SpatialPointsDataFrame(coords=stepps[,c("x", "y")], data=stepps, proj4string = albers) # Create a spatial object
-stepps <- sp::spTransform(stepps, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") # spatial projection transformation
-stepps <- data.frame(stepps) # Pull out the data frame
-names(stepps)[which(names(stepps) %in% c("x.1", "y.1"))] <- c("lon", "lat")
+# albers <- sp::CRS("+proj=aea +lat_1=42.122774 +lat_2=49.01518 +lat_0=45.568977 +lon_0=-83.248627 +x_0=1000000 +y_0=1000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs") # Define the spatial projection
+# stepps <- sp::SpatialPointsDataFrame(coords=stepps[,c("x", "y")], data=stepps, proj4string = albers) # Create a spatial object
+# stepps <- sp::spTransform(stepps, "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") # spatial projection transformation
+# stepps <- data.frame(stepps) # Pull out the data frame
+# names(stepps)[which(names(stepps) %in% c("x.1", "y.1"))] <- c("lon", "lat")
+
+# Change names to match up with drivers
 names(stepps)[which(names(stepps)=="sig")] <- c("n.sig")
 
 stepps$fract.sig <- stepps$n.sig/10
@@ -78,11 +93,11 @@ summary(stepps)
 
 # -----------
 # ReFAB (empirical biomass, centennially-resolved)
-# # NOTE: Derivative needs to be the mean ABSOLUTE VALUE!  NEEDS TO BE RE-RUN!!
 # -----------
-refab <- read.csv(file.path(path.google, "Current Data/Stability_GAMs", "Stability_ReFAB.csv"))
-refab$refab.mean.slope <- abs(refab$refab.mean.slope) # NOTE: SHOULDN"T HAVE TO DO THIS! FIX IN FUTURE VERSIONS
-names(refab) <- c("X", "lat", "lon", "deriv.abs", "n.sig")
+refab <- read.csv(file.path(path.google, "Current Data", "refab.mean.slope.csv"))
+names(refab) <- c("X", "lat", "lon", "deriv.mean", "deriv.abs", "n.sig")
+summary(refab)
+
 
 refab$fract.sig <- refab$n.sig/10
 refab$model <- as.factor("ReFAB")
@@ -160,6 +175,7 @@ dat.all <- rbind(lbda     [,cols.bind],
                  models1  [,cols.bind], 
                  # models.bm[,cols.bind],
                  fcomp    [,cols.bind])
+dat.all <- dat.all[!is.na(dat.all$lon),]
 summary(dat.all)
 
 # Getting a relative stability so that we can ignore actual value and just look at spatial patterning
@@ -179,6 +195,30 @@ for(mod in unique(dat.all$model)){
 # Exploratory graphing of datasets
 # -------------------------------------------
 us <- map_data("state")
+
+
+# -----------
+# Maping Datasets for comparison
+# -----------
+png(file.path(path.google, "Current Figures/Stability_Synthesis", "Model_v_Data_Spatial_Extent.png"), height=5, width=10, units="in", res=320)
+ggplot() +
+  geom_tile(data=drivers.all, aes(x=lon, y=lat, fill=tair.yr.set-273.15)) +
+  geom_path(data=us,aes(x=long, y=lat, group=group), color="gray50") + 
+  geom_point(data=stepps[stepps$pft=="OTHER.HARDWOOD",], aes(x=lon, y=lat, color="STEPPS"), shape=19, size=0.75, alpha=0.75) +
+  geom_point(data=refab, aes(x=lon, y=lat, color="ReFAB"), size=1.75) +
+  scale_color_manual(name="Pollen Data", values=c("black", "darkgoldenrod3")) +
+  scale_fill_gradient2(name="Mean Temp \n(1800-1850)", low = "blue", high = "red", mid = "white", midpoint = mean(drivers.all$tair.yr.set-273.15, na.rm=T)) +
+  labs(x="Longitude", y="Latitude") +
+  # guides(color=guide_legend(aes.overide=list(size=10)))+
+  # guides(shape=guide_legend(aes.override=list(size=20))) +
+  coord_equal(xlim=range(drivers.all$lon), ylim=range(drivers.all$lat), expand=0) +
+  theme(legend.position="top",
+        legend.key=element_blank(),
+        panel.background = element_rect(fill="gray50"),
+        panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank())
+dev.off()
+# -----------
 
 
 # -----------
@@ -258,7 +298,7 @@ ggplot(data=dat.all[dat.all$var=="biomass" & lat.lon.ind,]) +
   ggtitle("Centennial-Scale Climate Stability: empirical v. model")
 dev.off()
 
-png(file.path(path.google, "Current Figures/Stability_Synthesis", "Model_v_Data_Biomass_deriv_rel.png"), height=10, width=6, units="in", res=320)
+png(file.path(path.google, "Current Figures/Stability_Synthesis", "Model_v_Data_Biomass_deriv_rel.png"), height=5, width=10, units="in", res=320)
 ggplot(data=dat.all[dat.all$var=="biomass" & lat.lon.ind,]) +
   facet_wrap(~model) +
   geom_point(aes(x=lon, y=lat, color=deriv.rel)) +
@@ -330,6 +370,133 @@ for(mod in unique(dat.all[dat.all$class=="composition","model"])){
   )
 }
 dev.off()
+# -----------
+
+# -------------------------------------------
+
+
+# -------------------------------------------
+# Model-Data Comparisons: Model ~ Data
+# -------------------------------------------
+# Coming up with a table of the coordinates for the models
+coords.models <- drivers.all[!is.na(drivers.all$umw), c("lon", "lat")]
+summary(coords.models)
+
+# -----------
+# Biomass first (because it's easiest)
+# -----------
+for(i in 1:nrow(refab)){
+  # Find out which model grid point is closest to the refab site
+  lon.dist <- refab$lon[i] - coords.models$lon
+  lat.dist <- refab$lat[i] - coords.models$lat
+  ind.models <- which(sqrt(lon.dist^2 + lat.dist^2)==min(sqrt(lon.dist^2 + lat.dist^2)))
+
+  models1[models1$lon==coords.models$lon[ind.models] & models1$lat==coords.models$lat[ind.models], "refab"] <- refab$deriv.abs[i]
+}
+summary(models1)
+
+png(file.path(path.google, "Current Figures/Stability_Synthesis", "Model_v_Data_DerivAbs_Biomass.png"), height=6, width=6, units="in", res=320)
+ggplot(data=models1) +
+  geom_point(aes(x=refab, y=deriv.abs, color=model), size=2) +
+  stat_smooth(aes(x=refab, y=deriv.abs, color=model, fill=model), method="lm") +
+  geom_abline(intercept=0, slope=1, color="black", linetype="dashed") +
+  coord_cartesian(ylim=c(0,0.2)) +
+  theme_bw()
+dev.off()
+
+refab.ed <- lm(refab ~ deriv.abs, data=models1[models1$model=="ED2",])
+summary(refab.ed)
+
+refab.lpjg <- lm(refab ~ deriv.abs, data=models1[models1$model=="LPJ-GUESS",])
+summary(refab.lpjg)
+
+refab.lpjw <- lm(refab ~ deriv.abs, data=models1[models1$model=="LPJ-WSL",])
+summary(refab.lpjw)
+
+refab.link <- lm(refab ~ deriv.abs, data=models1[models1$model=="LINKAGES",])
+summary(refab.link)
+# -----------
+
+
+# -----------
+# STEPPS Composition: 
+# - look at only only compare dominant PFTs
+# -----------
+summary(stepps)
+coords.stepps <- stepps[stepps$pft=="OTHER.HARDWOOD", c("lon", "lat")]
+
+# create a dataframe where we'll store all of the fcomp
+fcomp.stab <- coords.models
+fcomp.stab$model <- paste(unique(fcomp$model)[1])
+for(mod in unique(fcomp$model)){
+  # If we need to add the model, do so first
+  if(length(which(fcomp.stab$model==mod))==0) fcomp.stab <- rbind(fcomp.stab, data.frame(coords.models, model=mod, pft.abs=NA, deriv.abs=NA))
+  
+  for(lon in unique(coords.models$lon)){
+    for(lat in unique(coords.models[coords.models$lon==lon, "lat"])){
+      df.fcomp <- fcomp[fcomp$model==mod & fcomp$lon==lon & fcomp$lat==lat,]
+      if(nrow(df.fcomp)==0) next
+      
+      # Make sure to get rid of the "total" column
+      df.fcomp <- df.fcomp[df.fcomp$pft!="Total",]
+      
+      ind.fcomp <- which(df.fcomp$value==max(df.fcomp$value))[1]
+      ind.stab <- which(fcomp.stab$model==mod & fcomp.stab$lon==lon & fcomp.stab$lat==lat)
+      
+      fcomp.stab[ind.stab, "pft.abs"] <- df.fcomp$pft[ind.fcomp]
+      fcomp.stab[ind.stab, "deriv.abs"] <- df.fcomp$deriv.abs[ind.fcomp]
+    }
+  }
+}
+fcomp.stab$model <- as.factor(fcomp.stab$model)
+fcomp.stab$pft.abs <- as.factor(fcomp.stab$pft.abs)
+summary(fcomp.stab)
+
+for(i in 1:nrow(coords.stepps)){
+  # Find out which model grid point is closest to the STEPPS site
+  lon.stepps <- coords.stepps$lon[i]
+  lat.stepps <- coords.stepps$lat[i]
+
+  lon.dist <- lon.stepps - coords.models$lon
+  lat.dist <- lat.stepps - coords.models$lat
+  ind.models <- which(sqrt(lon.dist^2 + lat.dist^2)==min(sqrt(lon.dist^2 + lat.dist^2)))
+  
+  lon.models <- coords.models$lon[ind.models]
+  lat.models <- coords.models$lat[ind.models]
+  
+  # Figure out which STEPPS PFT to pull
+  stepps.now <- stepps[stepps$lon==lon.stepps & stepps$lat==lat.stepps,]
+  stepps.now <- stepps.now[!stepps.now$pft %in% c("Deciduous", "Evergreen"),]
+  stepps.ind <- which(stepps.now$value==max(stepps.now$value, na.rm=T))[1]
+  # stepps.ind <- which(stepps.now$value==max(stepps.now$value, na.rm=T))[1]
+  
+  fcomp.stab[fcomp.stab$lon==lon.models & fcomp.stab$lat==lat.models, "pft.stepps"] <- stepps.now$pft[stepps.ind]
+  fcomp.stab[fcomp.stab$lon==lon.models & fcomp.stab$lat==lat.models, "stepps"] <- ifelse(is.na(stepps.now$deriv.abs[stepps.ind]), 0, stepps.now$deriv.abs[stepps.ind])
+}
+summary(fcomp.stab)
+
+png(file.path(path.google, "Current Figures/Stability_Synthesis", "Model_v_Data_DerivAbs_Fcomp.png"), height=6, width=6, units="in", res=320)
+ggplot(data=fcomp.stab) +
+  geom_point(aes(x=stepps, y=deriv.abs, color=model), size=2) +
+  stat_smooth(aes(x=stepps, y=deriv.abs, color=model, fill=model), method="lm") +
+  geom_abline(intercept=0, slope=1, color="black", linetype="dashed") +
+  # coord_cartesian(ylim=c(0,0.2)) +
+  theme_bw()
+dev.off()
+
+stepps.ed <- lm(stepps ~ deriv.abs, data=fcomp.stab[fcomp.stab$model=="ED2",])
+summary(stepps.ed)
+
+stepps.lpjg <- lm(stepps ~ deriv.abs, data=fcomp.stab[fcomp.stab$model=="LPJ-GUESS",])
+summary(stepps.lpjg)
+
+stepps.lpjw <- lm(stepps ~ deriv.abs, data=fcomp.stab[fcomp.stab$model=="LPJ-WSL",])
+summary(stepps.lpjw)
+
+stepps.trif <- lm(stepps ~ deriv.abs, data=fcomp.stab[fcomp.stab$model=="TRIFFID",])
+summary(stepps.trif)
+
+
 # -----------
 
 # -------------------------------------------
