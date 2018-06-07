9116ea5113854e2c10812d633c525e1f683c012f
Christy Rollinson <crollinson@gmail.com>
2018-06-07 13:35:12 -0500

    Calculate stability for 850-1850 AND LBDA time
    Will need updates from Ann & Andria, but at least we have the bones now.

--- scripts/2_biomass-data-stability.R
+++ scripts/2_biomass-data-stability.R
@@ -1,6 +1,8 @@
+path.google <- "/Volumes/GoogleDrive/My Drive/PalEON_ecosystem-change_models-vs-data/"
 
-load('/Volumes/GoogleDrive/My Drive/PalEON_ecosystem-change_models-vs-data/Current Data/ReFAB.all.samps.list.Rdata')
-load('/Volumes/GoogleDrive/My Drive/PalEON_ecosystem-change_models-vs-data/Current Data/refab.sites.lat.lon.Rdata')
+# Load data from Google Drive
+load(file.path(path.google, 'Current Data/raw_data/ReFAB/ReFAB.all.samps.list.Rdata'))
+load(file.path(path.google, 'Current Data/raw_data/ReFAB/refab.sites.lat.lon.Rdata'))
 
 ### Andria's function for calculating significance
 prob_sig <- function(x, prob){
@@ -14,16 +16,20 @@ prob_sig <- function(x, prob){
 }
 
 ### loop for getting mean differences and significance for all refab sites
-diff.mat.list <- list()
-refab.diff.mean <- sig_vals <- matrix(NA, 62, 99)
-for(i in 1:62){
-  if(i == 2){
+mean.mat.list <- diff.mat.list <- list()
+refab.mean <- refab.diff.mean <- sig_vals <- matrix(NA, 62, 99)
+for(i in 1:length(all.samps.list)){
+  if(length(all.samps.list[[i]]) == 0){
+    mean.mat.list[[i]] <- NULL #My dataset is currently missing billy's lake which is in position 2. I can remove this when Billy's Lake is added.
     diff.mat.list[[i]] <- NULL #My dataset is currently missing billy's lake which is in position 2. I can remove this when Billy's Lake is added.
   } else {
-    diff.mat.list[[i]] <- apply(all.samps.list[[i]], 1, function(x) diff(rev(x)/100, na.rm=TRUE)) 
+    mean.mat.list[[i]] <- apply(all.samps.list[[i]], 1, mean, na.rm=FALSE) 
+    diff.mat.list[[i]] <- apply(all.samps.list[[i]], 1, function(x) diff(rev(x)/100, na.rm=FALSE)) 
     # dividing by 100 because estimates are cenntennial. 
     # rev() because they are not sequential in time b[1] is present b[100] is 10,000 years BP
+    # From here out values go from past to present
     
+    refab.mean[i,] <- mean(mean.mat.list[[i]])
     refab.diff.mean[i,] <- rowMeans(diff.mat.list[[i]])
     
     sig_vals[i,] <- apply(t(diff.mat.list[[i]]), c(2), prob_sig, prob=.85)
@@ -38,17 +44,52 @@ points(refab.diff.mean[1,] ,col='red')
 
 ### summing accross time for significance
 time.bin <- 90:99 ## 850 - 1850 AD : 1000 - 100 years before present
-diff.mean = rowMeans(refab.diff.mean[,time.bin],na.rm = TRUE)
-diff.mean.abs = rowMeans(abs(refab.diff.mean[,time.bin]),na.rm = TRUE)
-n.signif = apply(sig_vals[,time.bin], 1, function(x) sum(!is.na(x)))
+mean.all = rowMeans(refab.mean[,time.bin],na.rm = F)
+diff.mean.all = rowMeans(refab.diff.mean[,time.bin],na.rm = F)
+diff.mean.abs.all = rowMeans(abs(refab.diff.mean[,time.bin]),na.rm = F)
+n.signif.all = apply(sig_vals[,time.bin], 1, function(x) sum(!is.na(x)))
 
 lat.lon.df.missing <- matrix(NA,62,2)
 lat.lon.df.missing[1,] <- as.numeric(lat.lon.df[1,])
 lat.lon.df.missing[3:62,] <- as.matrix(lat.lon.df[2:61,])
 
 refab.mean.slope = data.frame(lat = lat.lon.df.missing[,1], lon = lat.lon.df.missing[,2],
-                            refab.mean.slope =diff.mean,
-                            refab.mean.slope.abs = diff.mean.abs,
-                            n.signif = n.signif)
+                              refab.mean.1k = mean.all,
+                              refab.mean.slope.1k =diff.mean.all,
+                              refab.mean.slope.abs.1k = diff.mean.abs.all,
+                              n.signif.1k = n.signif.all)
 
-write.csv(refab.mean.slope, file = '~/Google Drive/PalEON_ecosystem-change_models-vs-data/refab.mean.slope.csv')
+# Adding in the stability just for the period for which we have climate data
+lbda.df <- read.csv(file.path(path.google, "Current Data/Stability_GAMs", "Stability_LBDA_100.csv"))
+summary(lbda.df)
+
+# Loop through each point and get the number of points we should look at
+for(i in 1:nrow(refab.mean.slope)){
+  if(is.na(refab.mean.slope$lat[i])) next 
+  
+  # Find the closest LDBA grid cell
+  lon.refab <- refab.mean.slope$lon[i]
+  lat.refab <- refab.mean.slope$lat[i]
+  
+  lon.dist <- lon.refab - lbda.df$lon
+  lat.dist <- lat.refab - lbda.df$lat
+  ind.lbda <- which(sqrt(lon.dist^2 + lat.dist^2)==min(sqrt(lon.dist^2 + lat.dist^2)))
+  
+  # if(length(ind.lbda)>1) ind.lbda <- ind.libda[1] # If a point is equidistant
+  
+  # Get the number of years to look at by rounding to the nearest 100
+  n.yrs <- round(mean(lbda.df$n.yrs[ind.lbda]), -2) # Adding mean() in case a site is equidistant between lbda points
+  
+  if(is.na(n.yrs)) next # Skip anythign that's not in an LBDA grid cell for some reason
+  
+  time.bin <- 99:(99-n.yrs/100) # Updating our time bin based on number of years
+  
+  # Extracting/storing the info for the LBDA period
+  refab.mean.slope[i,"n.yrs.lbda"] <- n.yrs
+  refab.mean.slope[i, "refab.mean.lbda"] <-  mean(refab.mean[i,time.bin],na.rm = F)
+  refab.mean.slope[i, "refab.mean.slope.lbda"    ] <-  mean(refab.diff.mean[i,time.bin],na.rm = F)
+  refab.mean.slope[i, "refab.mean.slope.abs.lbda"] <-  mean(abs(refab.diff.mean[i,time.bin]),na.rm = F)
+}
+summary(refab.mean.slope)
+
+write.csv(refab.mean.slope, file = file.path(path.google, "Current Data/Stability_Index", 'refab.stability.csv'))
---
+++ scripts/2b_Fcomp_stability_STEPPS_LBDAtime.R
@@ -0,0 +1,81 @@
+library(sp); library(rgdal)
+path.google <- "/Volumes/GoogleDrive/My Drive/PalEON_ecosystem-change_models-vs-data/"
+
+# Load data from Google Drive
+stepps <- readRDS(file.path(path.google, 'Current Data/raw_data/STEPPS/STEPPS_predictions_median_sd.RDS'))
+stepps$taxon <- as.factor(stepps$taxon)
+stepps$year <- 1950-stepps$time
+stepps <- stepps[stepps$year>=800,] # Subset to just the time slice we're interested for this
+summary(stepps)
+
+stepps.sp <- stepps[stepps$taxon=="OAK" & stepps$year==1750,c("cell", "x", "y")]
+# stepps.sp[,c("x", "y")] <- stepps.sp[,c("x", "y")]*1e6 # Andria had convereted to megameters
+stepps.sp <- SpatialPointsDataFrame(coords=stepps.sp[,c("x", "y")]*1e6, stepps.sp, proj4string = CRS("+init=epsg:3175")) # x,y * 1e6 to convert to meters
+stepps.sp <- spTransform(stepps.sp, CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+stepps.sp <- data.frame(stepps.sp)[,1:5]
+names(stepps.sp)[which(names(stepps.sp) %in% c("x.1", "y.1"))] <- c("lon", "lat")
+summary(stepps.sp)
+
+# Add lat/lon in
+stepps <- merge(stepps, stepps.sp)
+summary(stepps)
+
+# Adding in the stability just for the period for which we have climate data
+lbda.df <- read.csv(file.path(path.google, "Current Data/Stability_GAMs", "Stability_LBDA_100.csv"))
+summary(lbda.df)
+
+# Setting up a blank data frame to store stability metrics in
+stab.stepps <- data.frame(cell=rep(unique(stepps$cell), each=length(unique(stepps$taxon))),
+                          taxon=unique(stepps$taxon))
+head(stab.stepps)
+
+# Looping through each grid cell
+for(CELL in unique(stab.stepps$cell)){
+  # Extracting the stepps lat/lon
+  stepps.lon <- unique(stepps[stepps$cell==CELL,"lon"])
+  stepps.lat <- unique(stepps[stepps$cell==CELL,"lat"])
+  
+  stab.stepps[stab.stepps$cell==CELL, "lon"] <- stepps.lon
+  stab.stepps[stab.stepps$cell==CELL, "lat"] <- stepps.lat
+  
+  # finidng the closest LBDA Grid cell
+  lon.dist <- stepps.lon - lbda.df$lon
+  lat.dist <- stepps.lat - lbda.df$lat
+  ind.lbda <- which(sqrt(lon.dist^2 + lat.dist^2)==min(sqrt(lon.dist^2 + lat.dist^2)))
+  
+  # Get the number of years to look at by rounding to the nearest 100
+  yr.min <- 1850-mean(lbda.df$n.yrs[ind.lbda])
+  
+  stab.stepps[stab.stepps$cell==CELL,"lbda.min"]   <- yr.min
+  
+  # n.yrs <- round(mean(lbda.df$n.yrs[ind.lbda]), -2) # Adding mean() in case a site is equidistant between lbda points
+  # stab.stepps[stab.stepps$cell==CELL,"n.yrs.lbda"] <- n.yrs
+  # time.bin <- 99:(99-n.yrs/100) # Updating our time bin based on number of years
+  
+  
+  # Subset our cell data & order it through time  to speed things up
+  dat.cell <- stepps[stepps$cell==CELL,]
+  dat.cell <- dat.cell[order(dat.cell$year),]
+
+  # Looping through each PFT 
+  for(TAX in unique(stepps[stepps$cell==CELL, "taxon"])){
+    df.tmp <- dat.cell[dat.cell$cell==CELL & dat.cell$taxon==TAX,]
+    
+    # Looking at stability over the full 1k period (Model Sim period)
+    diff.1k <- diff(df.tmp$median)
+    stab.stepps[stab.stepps$cell==CELL & stab.stepps$taxon==TAX, "stepps.mean.1k"    ] <- mean(df.tmp$median)
+    stab.stepps[stab.stepps$cell==CELL & stab.stepps$taxon==TAX, "stepps.diff.1k"    ] <-  mean(diff.1k, na.rm = F)
+    stab.stepps[stab.stepps$cell==CELL & stab.stepps$taxon==TAX, "stepps.diff.abs.1k"] <-  mean(abs(diff.1k), na.rm = F)
+    
+    if(is.na(yr.min)) next # Skip anything that's not in an LBDA grid cell for some reason
+    
+    # Looking at stability just where we have empirical drought data
+    diff.lbda <- diff(df.tmp[df.tmp$year>=yr.min,"median"])
+    stab.stepps[stab.stepps$cell==CELL & stab.stepps$taxon==TAX, "stepps.mean.lbda"    ] <- mean(df.tmp[df.tmp$year>=yr.min,"median"])
+    stab.stepps[stab.stepps$cell==CELL & stab.stepps$taxon==TAX, "stepps.diff.lbda"    ] <-  mean(diff.lbda, na.rm = F)
+    stab.stepps[stab.stepps$cell==CELL & stab.stepps$taxon==TAX, "stepps.diff.abs.lbda"] <-  mean(abs(diff.lbda), na.rm = F)
+  } # End Taxon Loop
+} # End Grid Cell Loop
+summary(stab.stepps)
+
+write.csv(stab.stepps, file = file.path(path.google, "Current Data/Stability_Index", 'stepps.stability.csv'))
